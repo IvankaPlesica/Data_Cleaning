11ff39c8c389e45862d9db24a7ab547edea62181
IvankaPlesica <37933145+IvankaPlesica@users.noreply.github.com>
2024-02-22 08:26:13 +0100

    A little pandas exercise found online
    Mostly practicing functions for cleaning and standardizing strings.

--- Customer_Call_FIFA_Data_Cleaning.py
+++ Customer_Call_FIFA_Data_Cleaning.py
@@ -1,250 +1,111 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[110]:
-
+# This is a collection of a few common cleaning functions that usually come in handy.
+# The exercises were done mostly following an existing exercise online.
 
 import pandas as pd
 
-
-# In[3]:
-
-
 df = pd.read_excel(r"C:\Users\ivank\Downloads\Customer Call List.xlsx")
-df
 
-
-# In[6]:
-
-
+#This line deletes duplicate values.
 df = df.drop_duplicates()
-df
 
-
-# In[8]:
-
-
+#Removes a whole column that isn't useful.
 df = df.drop(columns = "Not_Useful_Column")
 
 
-# In[15]:
-
-
+#Different iterations of the strip function, used depending on investigating data.
 df["Last_Name"] = df["Last_Name"].str.lstrip("...")
 df["Last_Name"] = df["Last_Name"].str.lstrip("/")
 df["Last_Name"] = df["Last_Name"].str.rstrip("_")
-#df["Last_Name"] = df["Last_Name"].str.lstrip("123._/")
-df
+df["Last_Name"] = df["Last_Name"].str.lstrip("123._/")
 
-
-# In[14]:
-
-
-df
-
-
-# In[28]:
-
-
-#df["Phone_Number"]=df["Phone_Number"].str.replace('[^a-zA-Z0-9]','')
-#df["Phone_Number"] = df["Phone_Number"].apply(lambda x: str(x))
-#df["Phone_Number"] = df["Phone_Number"].apply(lambda x: x[0:3] + '-' + x[3:6] + '-' + x[6:10])
+#str.replace is being used to remove specific caracters by replacing them with ''.
+#lamdba function is being used to standardize formats; useful when we need to apply a function to a single column.
+df["Phone_Number"]=df["Phone_Number"].str.replace('[^a-zA-Z0-9]','')
+df["Phone_Number"] = df["Phone_Number"].apply(lambda x: str(x))
+df["Phone_Number"] = df["Phone_Number"].apply(lambda x: x[0:3] + '-' + x[3:6] + '-' + x[6:10])
 df["Phone_Number"] = df["Phone_Number"].str.replace('nan--','').replace('na--','').replace('Na--','')
 
-
-# In[34]:
-
-
+#Standard approach to separating an address into separate columns, based on different elements of an address.
 df[["Street_Address", "State", "Zip_Code"]] = df["Address"].str.split(',',2,expand = True)
 
-
-# In[55]:
-
-
+#Setting regex to False so it treats 'Y' literally and not as a regular expression pattern.
 df["Paying Customer"] = df["Paying Customer"].replace('Y', 'Yes', regex = False).replace('N', 'No')
-
-
-# In[58]:
-
-
 df["Do_Not_Contact"] = df["Do_Not_Contact"].replace('Y', 'Yes', regex = False).replace('N', 'No', regex = False)
 
-
-# In[59]:
-
-
-df
-
-
-# In[73]:
-
-
-#df["Paying Customer"] = df["Paying Customer"].replace('N/a', '', regex = False)
-#df["Do_Not_Contact"] = df["Do_Not_Contact"].replace('N/a', '', regex = False)
+#Different ways of replacing missing values with an empty string
+df["Paying Customer"] = df["Paying Customer"].replace('N/a', '', regex = False)
+df["Do_Not_Contact"] = df["Do_Not_Contact"].replace('N/a', '', regex = False)
 df = df.fillna('')
 
 
-# In[74]:
-
-
-df
-
-
-# In[98]:
-
-
+#Creating a new data frame based on an importan condition. Here, it separates customers based on their permission
+#to use ther given phone numbers
 df_call_list = df[(df["Do_Not_Contact"]  == 'No') & (df["Phone_Number"] != '')]
 
 
-# In[106]:
-
-
-#df_call_list.reset_index(drop = True)
-#df_call_list = df_call_list.drop("Address", axis = 1)
-#df = df.drop("Address", axis = 1)
+#Since drop usually operates on rows, we use axis to drop columns.
+df_call_list.reset_index(drop = True)
+df_call_list = df_call_list.drop("Address", axis = 1)
+df = df.drop("Address", axis = 1)
 df_call_list
 
 
-# # FIFA DATASET
+# # FIFA DATASET - a little exercise
 
 # ### Insert and inspect data; remove duplicates
 
-# In[162]:
-
-
 df = pd.read_csv(r"C:\Users\ivank\Downloads\archive\fifa21 raw data v2.csv")
-
-
-# In[163]:
-
-
-df.head(5)
-
-
-# In[164]:
-
-
 df.drop_duplicates()
 
 
 # ### Club
-
-# In[165]:
-
-
 df["Club"] = df["Club"].replace(r'\s+|\\n', '', regex = True)
 
 
-# In[166]:
+pd.set_option('display.max_columns', None)
 
-
-#pd.set_option('display.max_columns', None)
-
-
-# In[316]:
-
-
-#df["Club"] = df["Club"].str.strip()
-#df["Club"] = df["Club"].replace('FC', ' FC ', regex = True)
-#df["Club"] = df["Club"].str.findall('[A-Z]*[^A-Z]*')
-#df["Club"] = df["Club"].str.join(" ")
-#df["Club"] = df["Club"].replace("- ", "-", regex = True)
-#df["Club"] = df["Club"].str.strip()
-df.head(50)
+#Cleaning strings in data.
+df["Club"] = df["Club"].str.strip()
+df["Club"] = df["Club"].replace('FC', ' FC ', regex = True)
+df["Club"] = df["Club"].str.findall('[A-Z]*[^A-Z]*')
+df["Club"] = df["Club"].str.join(" ")
+df["Club"] = df["Club"].replace("- ", "-", regex = True)
+df["Club"] = df["Club"].str.strip()
 
 
 # ### Value
-
-# In[241]:
-
-
-#df["Value"] = df["Value"].str.lstrip('€')
-#df["Value"] = df["Value"].astype(float)
+#unifying value data types
+df["Value"] = df["Value"].str.lstrip('€')
+df["Value"] = df["Value"].astype(float)
 df["Value M/K"] = df["Value M/K"].astype(float)
 
-
-# In[169]:
-
-
-#df_value.unique()
+df_value.unique()
 df_value = df["Value"].str[-1].replace('M', 1000000).replace('K', 1000)
-
-
-# In[180]:
 
 
 df["Value"] = df["Value"].str.rstrip('K')
 df["Value"] = df["Value"].str.rstrip('M')
 
 
-# In[244]:
-
-
-#df["Value"].astype(float).multiply(df_value, axis = 0)
+df["Value"].astype(float).multiply(df_value, axis = 0)
 df["Value"] = (df["Value"] * df["Value M/K"]).astype(int)
 
 
-# In[245]:
-
-
-df
-
-
-# In[246]:
-
-
-df["Value"]
-
-
 # ### Wage
-
-# In[318]:
-
-
+#removing symbols from a wage to make it usable for calculations
 df["Wage"] = df["Wage"].str.lstrip('€')
-
-
-# In[320]:
-
-
 df["Wage"].unique()
 
-
-# In[326]:
-
-
-#df["Wage"] = df["Wage"].replace("K", "000", regex = True)
+#making the values usable for calculations
+df["Wage"] = df["Wage"].replace("K", "000", regex = True)
 df["Wage"] = df["Wage"].astype(int)
-
-
-# In[328]:
-
-
-df["Wage"].unique()
 
 
 # ### Release Clause
 
-# In[331]:
-
-
 df["Release Clause"] = df["Release Clause"].str.lstrip('€')
-
-
-# In[335]:
-
-
 df["Release Clause"].unique()
 
-
-# In[ ]:
-
-
-
-
-
-# In[317]:
-
-
-df["Value"]
 
